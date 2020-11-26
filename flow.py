8130d748704654b213b59b67d50a2873d38bbd06
Or Zilberman <zilbermanor@gmail.com>
2020-11-26 16:22:26 +0200

    Code cosmetics

--- flow.py
+++ flow.py
@@ -1,106 +1,15 @@
-from pprint import pprint
-import spacy
-from storey import build_flow, Source, Map, Filter, FlatMap, Reduce, Event
+from storey import build_flow, Source, Map, Filter, FlatMap
 from storey.dtypes import _termination_obj
-
-from sources import ReadJSON
-from flows import URLDownloader
+from flows import (
+    URLDownloader,
+    apply_nlp,
+    extract_entities,
+    enrich_entities,
+    to_paragraphs,
+)
 import os
-from typing import Union, List
 import cloudpickle
 
-
-def append_and_return(dct, x):
-    temp = dct.get(x.id, [])
-    dct[x.id] = temp + [x.body]
-    return dct
-
-
-def flow_v1():
-    flow = build_flow(
-        [
-            ReadJSON(["data/2.json", "data/1.json"]),
-            Map(lambda paragraph: nlp(paragraph)),
-            FlatMap(lambda doc: doc.ents),
-            Map(lambda ent: [ent.text, ent.start_char, ent.end_char, ent.label_]),
-            # Filter(lambda ent: ent[3] in ["PERSON", "ORG"]),
-            Reduce({}, lambda acc, x: append_and_return(acc, x), full_event=True)
-            # SendToHttp()
-        ]
-    ).run()
-
-    return flow
-
-
-def to_paragraphs(docs: Union[List, Event]):
-    if docs == _termination_obj or docs is None:
-        print("No doc")
-        return _termination_obj
-    else:
-        paragraphs = []
-        for doc in docs:
-            for i, paragraph in enumerate(doc["doc"]):
-                paragraphs.append(
-                    [{"url": doc["url"], "paragraph_id": i, "paragraph": paragraph}]
-                )
-        return paragraphs
-
-
-def apply_nlp(paragraphs: List[dict]):
-    if paragraphs == _termination_obj:
-        return _termination_obj
-    else:
-        tokenized = []
-        for paragraph in paragraphs:
-            tokenized.append(
-                {
-                    "url": paragraph["url"],
-                    "paragraph_id": paragraph["paragraph_id"],
-                    "tokens": nlp(paragraph["paragraph"]),
-                }
-            )
-
-        return tokenized
-
-
-def extract_entities(tokens):
-    if tokens == _termination_obj:
-        return _termination_obj
-    else:
-        paragraph_entities = []
-        for token_paragraph in tokens:
-            entities = token_paragraph["tokens"].ents
-            for entity in entities:
-                paragraph_entities.append(
-                    {
-                        "url": token_paragraph["url"],
-                        "paragraph_id": token_paragraph["paragraph_id"],
-                        "entity": entity.ents,
-                    }
-                )
-        return paragraph_entities
-
-
-def enrich_entities(entities):
-    if entities == _termination_obj:
-        return _termination_obj
-    else:
-        enriched_entities = []
-        for entity in entities["entity"]:
-            enriched_entities.append(
-                {
-                    "url": entities["url"],
-                    "paragraph_id": entities["paragraph_id"],
-                    "entity_text": entity.text,
-                    "entity_start_char": entity.start_char,
-                    "entity_end_char": entity.end_char,
-                    "entity_label": entity.label_,
-                }
-            )
-        return enriched_entities
-
-
-nlp = spacy.load("en_core_web_sm")
 secrets = cloudpickle.load(open("secrets.pkl", "rb"))
 os.environ.update(secrets)
 
@@ -109,7 +18,7 @@ flow = build_flow(
         Source(),
         URLDownloader(secrets=secrets),  # Get URL and download
         FlatMap(to_paragraphs),  # extracts Disct[...list_path_in_json...] to List
-        Map(apply_nlp),
+        apply_nlp(spacy_dict="en_core_web_sm"),
         FlatMap(extract_entities),
         Map(enrich_entities),
         Filter(lambda ent: ent[0]["entity_label"] in ["PERSON", "ORG"]),
@@ -121,6 +30,6 @@ flow = build_flow(
 
 flow.emit("s3://igz-downloads/1.json")
 flow.emit("s3://igz-downloads/2.json")
-# flow.emit(["s3://igz-downloads/2.json", "s3://igz-downloads/1.json"])
+flow.emit(["s3://igz-downloads/2.json", "s3://igz-downloads/1.json"])
 flow.emit("https://igz-downloads.s3.amazonaws.com/2.json")
 flow.emit(_termination_obj)
--- flows.py
+++ flows.py
@@ -1,19 +1,15 @@
 from storey import MapClass, Event
 from storey.dtypes import _termination_obj
 from mlrun import get_object
-from mlrun.utils import Logger
 from typing import Union, List
 import json
+import spacy
 
 
 class URLDownloader(MapClass):
     def __init__(self, prefix: str = None, secrets: dict = None):
 
         super().__init__()
-
-        # Set mlrun context
-        # self.logger = Logger(1, name="URLDownloader")
-        # self.logger.info("URLDownloader online")
         self.secrets = secrets
 
     def do(self, urls: Union[str, Event, List]):
@@ -24,7 +20,8 @@ class URLDownloader(MapClass):
             url_list = [urls]
         elif urls == _termination_obj:
             return self._do_downstream(_termination_obj)
-        # self.logger.debug(f"Handling {urls}")
+        else:
+            url_list = urls
         docs = []
         for url in url_list:
             try:
@@ -32,5 +29,78 @@ class URLDownloader(MapClass):
                 docs.append({"url": url, "doc": doc})
             except Exception as e:
                 print(f"Couldnt get {url}, {e}")
-                # self.logger.warn(f"Couldnt get {url}, {e}")
         return docs
+
+
+def to_paragraphs(docs: Union[List, Event]):
+    if docs == _termination_obj or docs is None:
+        print("No doc")
+        return _termination_obj
+    else:
+        paragraphs = []
+        for doc in docs:
+            for i, paragraph in enumerate(doc["doc"]):
+                paragraphs.append(
+                    [{"url": doc["url"], "paragraph_id": i, "paragraph": paragraph}]
+                )
+        return paragraphs
+
+
+class apply_nlp(MapClass):
+    def __init__(self, spacy_dict="en_core_web_sm"):
+        super().__init__()
+
+        self.nlp = spacy.load(spacy_dict)
+
+    def do(self, paragraphs: List[dict]):
+        if paragraphs == _termination_obj:
+            return _termination_obj
+        else:
+            tokenized = []
+            for paragraph in paragraphs:
+                tokenized.append(
+                    {
+                        "url": paragraph["url"],
+                        "paragraph_id": paragraph["paragraph_id"],
+                        "tokens": self.nlp(paragraph["paragraph"]),
+                    }
+                )
+
+            return tokenized
+
+
+def extract_entities(tokens):
+    if tokens == _termination_obj:
+        return _termination_obj
+    else:
+        paragraph_entities = []
+        for token_paragraph in tokens:
+            entities = token_paragraph["tokens"].ents
+            for entity in entities:
+                paragraph_entities.append(
+                    {
+                        "url": token_paragraph["url"],
+                        "paragraph_id": token_paragraph["paragraph_id"],
+                        "entity": entity.ents,
+                    }
+                )
+        return paragraph_entities
+
+
+def enrich_entities(entities):
+    if entities == _termination_obj:
+        return _termination_obj
+    else:
+        enriched_entities = []
+        for entity in entities["entity"]:
+            enriched_entities.append(
+                {
+                    "url": entities["url"],
+                    "paragraph_id": entities["paragraph_id"],
+                    "entity_text": entity.text,
+                    "entity_start_char": entity.start_char,
+                    "entity_end_char": entity.end_char,
+                    "entity_label": entity.label_,
+                }
+            )
+        return enriched_entities
